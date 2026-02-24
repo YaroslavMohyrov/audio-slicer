5152609087efff2256f1707e44f45be5a5d0a79a
Yaroslav Mohyrov <y.mohyrov@gmail.com>
2026-02-24 22:13:22 +0200

    chore: setup alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import path from 'path'
 
 export default defineConfig({
   base: "/audio-slicer/",
   plugins: [solid()],
+  resolve: {
+    alias: {
+      '@lib': path.resolve(__dirname, './src/lib'),
+    },
+  },
 })
